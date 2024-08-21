6789cbd50fa5178186c200e5963ffab5e176546b
leonid-06 <harinlenya1488@gmail.com>
2024-08-21 12:03:49 +0300

    security out of box

---
+++ src/main/java/com/leonid/springsecurityunderstanding/SpringSecurityUnderstandingApplication.java
@@ -0,0 +1,13 @@
+package com.leonid.springsecurityunderstanding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityUnderstandingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityUnderstandingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leonid/springsecurityunderstanding/SpringSecurityUnderstandingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonid.springsecurityunderstanding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityUnderstandingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
